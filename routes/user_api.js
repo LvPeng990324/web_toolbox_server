2d97cd0e4f5edee0d8fae26506824c6749a367f3
Lv Peng <lvpeng990324@qq.com>
2025-05-24 16:06:50 +0800

    优化结构

--- routes/user_api.js
+++ routes/user_api.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {RETURN_CODE, json_response} = require("../utils/response");
 
 // 登录
 router.post("/login", async (req, res) => {
     const {username, password} = req.body;
     if (username == 'guest') {
-        json_response(res, RETURN_CODE.success, "登录成功", {
+        ms.response.json_response(res, ms.response.RETURN_CODE.success, "登录成功", {
             token: "token-guest",
         });
         return;
     }
 
-    json_response(res, RETURN_CODE.success, "登录成功", {
+    ms.response.json_response(res, ms.response.RETURN_CODE.success, "登录成功", {
         token: "token-admin",
     });
 });
@@ -21,7 +20,7 @@ router.post("/login", async (req, res) => {
 router.get("/get-user-info", async (req, res) => {
     const username = req.headers.authorization.split('token-')[1]
 
-    ms.response.json_response(res, RETURN_CODE.success, "获取成功", {
+    ms.response.json_response(res, ms.response.RETURN_CODE.success, "获取成功", {
         username: username,
     });
 });
